a741c0b96bb25bf640f98b013783966d9b431efc
Sean Caron <scaron@umich.edu>
2022-01-06 14:54:11 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,3 +27,6 @@ cp run_collection $INSTALL_DIR/diskmonitor/bin
 
 cp diskmonitor_server.conf $INSTALL_DIR/diskmonitor/etc
 cp dashboard.ini $INSTALL_DIR/diskmonitor/etc
+
+mkdir -p /usr/lib/cgi-bin/diskmonitor
+cp diskmonitor.py /usr/lib/cgi-bin/diskmonitor/diskmonitor
