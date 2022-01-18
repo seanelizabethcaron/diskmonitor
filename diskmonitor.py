fdeb107cee43592fb1e4a18d8efd06e184379a84
Sean Caron <scaron@umich.edu>
2022-01-17 20:20:25 -0500

    Gather a few statistics while looping over drives

--- diskmonitor.py
+++ diskmonitor.py
@@ -23,6 +23,10 @@ dbpass = cfg.get('database', 'passwd')
 dbname = cfg.get('database', 'db')
 dbhost = cfg.get('database', 'host')
 
+total_disks = 0
+yellow_disks = 0
+red_disks = 0
+
 db = MySQLdb.connect(host=dbhost,user=dbuser,passwd=dbpass,db=dbname)
 
 curs = db.cursor()
@@ -50,18 +54,21 @@ for host in hosts:
     toggle = 0
 
     displayhost = host[0].replace("_", "-")
-    
+
     print('<p><a name=\"' + displayhost + '\">' + displayhost + '</a></p>')
     print('<table>')
     print('<tr><th>device</th><th>memberof_array</th><th>smart_health</th><th>raw_rd_err_rt</th><th>realloc_sec_ct</th><th>realloc_ev_ct</th><th>current_pending_ct</th><th>offline_uncorr_ct</th><th>udma_crc_err_ct</th></tr>')
     for row in disks:
         if 'FAIL' in row[3]:
             print('<tr bgcolor=#ffcccc><td>')
+            red_disks = red_disks + 1
         elif row[5] > 0 or row[6] > 0 or row[7] > 0 or row[8] > 0:
             if row[5] > 100 or row[6] > 100 or row[7] > 100 or row[8] > 100:
                 print('<tr bgcolor=#ffcccc><td>')
+                red_disks = red_disks + 1
             else:
                 print('<tr bgcolor=#ffffcc><td>')
+                yellow_disks = yellow_disks + 1
         else:
             if toggle == 0:
                 print('<tr bgcolor=#ccffcc><td>')
@@ -88,8 +95,18 @@ for host in hosts:
         print('</td><tr>')
 
         toggle = not toggle
+        total_disks = total_disks + 1
 
     print('</table>')
+
+print('<font face="Arial, Helvetica">')
+print('<br>')
+print('Total disks monitored: ' + str(total_disks))
+print('<br>')
+print('Total disks in RED status: ' + str(red_disks))
+print('<br>')
+print('Total disks in YELLOW status: ' + str(yellow_disks))
+print('</font>')
 
 print('</body>')
 print('</html>')
